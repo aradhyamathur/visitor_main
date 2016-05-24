ff888445632c51218e1a0e50553909650c712525
aradhyamathur <aradhya.mathur95@gmail.com>
2016-05-24 22:08:21 +0530

    third commit - Phone number field changes

--- visitorcreator/models.py
+++ visitorcreator/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models as md
 from django.core.validators import EmailValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -12,7 +13,7 @@ class Visitor(md.Model):
     name = md.CharField(max_length=60)
     email = md.CharField(max_length=255, validators=[EmailValidator])
     photo = md.ImageField(blank=True)
-    phone_number = md.BigIntegerField()
+    phone_number = PhoneNumberField()
     visitor_type = md.CharField(max_length=20, null=True)
     company_to_visit = md.CharField(max_length=60, null=True, blank=True)
     employee_to_visit = md.CharField(max_length=60, null=True, blank=True)
